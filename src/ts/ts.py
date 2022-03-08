507f7258242c81e58450885bc03894762f8523ec
Andreas <akbr18@student.aau.dk>
2022-03-08 11:08:14 +0100

    fix (ts): TreeCursor and not Cursor

--- src/ts/ts.py
+++ src/ts/ts.py
@@ -16,10 +16,6 @@ class Range:
     def __init__(self) -> None:
         pass
 
-class Cursor:
-    def __init__(self) -> None:
-        pass
-
 class Node:
     def __init__(self, node: _Node) -> None:
         self._node = node
@@ -174,7 +170,7 @@ class Tree:
             new_end_point,
         )
 
-    def walk(self) -> Cursor:
+    def walk(self) -> TreeCursor:
         return TreeCursor(self._tree.walk())
 
 class Parser:
@@ -212,8 +208,8 @@ class Query:
         """
         self._query(node._node)
 
-#    def captures(self, node: Node) -> List[Tuple[Node, str]]:
-#        return self._query.captures(node)
+    def captures(self, node: Node) -> List[Tuple[Node, str]]:
+        return self._query.captures(node)
 
 class Language:
     def __init__(self, language: _Language) -> None:
