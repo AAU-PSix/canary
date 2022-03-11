20470b50ee10570b6309f6f3bf04ed690c5a7c94
Brandhoej <akbr18@student.aau.dk>
2022-03-11 17:23:52 +0100

    test (obom): oaba, oaea, oasa

--- src/mutation_analyser/test_mutation_analyser.py
+++ src/mutation_analyser/test_mutation_analyser.py
@@ -1,4 +1,5 @@
-from sre_constants import SRE_FLAG_LOCALE
+from sys import float_info
+from typing import Dict
 from . import *
 
 import unittest
@@ -14,12 +15,39 @@ class TestMutationAnalyser(unittest.TestCase):
         self._binary_expression_query: Query = self._language.query("((binary_expression) @exp)")
         return super().setUp()
 
-    def test_obom_aritmetic_assignment_domain_bitwise_assignment_range(self) -> None:
-        root: Node = self._parser.parse("a+=b").root_node
-        captures: List[Tuple[Node, str]] = self._compound_assignment_query.captures(root)
-        operator: Node = captures[0][0].children[1]
+    def parse_first_augmented_assignment_expression_operator(self, expression: str) -> Node:
+            root: Node = self._parser.parse(expression).root_node
+            captures: List[Tuple[Node, str]] = self._compound_assignment_query.captures(root)
+            operator: Node = captures[0][0].children[1]
+            return operator
 
-        actual: str = self._mutator.obom(operator, 0, 0)
+    def parse_first_binary_expression_operator(self, expression: str) -> Node:
+            root: Node = self._parser.parse(expression).root_node
+            captures: List[Tuple[Node, str]] = self._binary_expression_query.captures(root)
+            operator: Node = captures[0][0].children[1]
+            return operator
 
-        self.assertEqual(operator.type, '+=')
-        self.assertEqual(actual, '|=')
+    def test_obom_ranges_oaba_oaea_oasa(self) -> None:
+        domain: List[str] = self._language.arithmetic_compound_assignment
+        range: List[tuple(str, float, float)] = [
+            # OABA
+            ("|=", 0, 0),
+            ("|=", 0, 1/3 - float_info.epsilon),
+            ("&=", 0, 1/3),
+            ("&=", 0, 2/3 - float_info.epsilon),
+            ("^=", 0, 2/3),
+            ("^=", 0, 1 - float_info.epsilon),
+            # OAEA
+            ("=", 1/3, 0),
+            ("=", 1/3, 1 - float_info.epsilon),
+            # OASA
+            ("<<=", 2/3, 0),
+            (">>=", 2/3, 1 - float_info.epsilon),
+        ]
+        for domain_operator in domain:
+            operator: Node = self.parse_first_augmented_assignment_expression_operator(f'a{domain_operator}b')
+
+            for range_section in range:
+                actual: str = self._mutator.obom(operator, range_section[1], range_section[2])
+                self.assertEqual(operator.type, domain_operator)
+                self.assertEqual(actual, range_section[0])
