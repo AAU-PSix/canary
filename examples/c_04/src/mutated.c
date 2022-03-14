169c8acacbe153fe73b3132ea7070d2ce16320ca
Andreas <akbr18@student.aau.dk>
2022-03-14 11:28:11 +0100

    fix (gh actions): Moved away from the black tool to action

--- examples/c_04/src/mutated.c
+++ examples/c_04/src/mutated.c
@@ -7,13 +7,13 @@ int main() {
   scanf("%lf", &second);
 
   // value of first is assigned to temp
-  temp %= first;
+  temp = first;
 
   // value of second is assigned to first
   first = second;
 
   // value of temp (initial value of first) is assigned to second
-  second = temp;
+  second += temp;
 
   // %.2lf displays number up to 2 decimal points
   printf("\nAfter swapping, first number = %.2lf\n", first);
