1b1a1e001ca36150236e8cfa49d7d0489f821f61
Andreas <akbr18@student.aau.dk>
2022-03-14 11:19:14 +0100

    cleanup (main): Removed prints

--- src/__main__.py
+++ src/__main__.py
@@ -18,20 +18,15 @@ def main():
     input: str = args.input
     output: str = args.output
 
-    print(input)
-    print(output)
-
     input_file: FileHandler = open(input, "r")
     input_contents: str = input_file.read()
     input_file.close()
 
     parser: Parser = Parser.c()
     tree: Tree = parser.parse(input_contents)
-    print(tree.text)
 
     mutator = Mutator(parser)
     mutated_tree: Tree = mutator.mutate(tree)
-    print(mutated_tree.text)
 
     output_file: str = open(output, "w+")
     output_file.write(mutated_tree.text)
