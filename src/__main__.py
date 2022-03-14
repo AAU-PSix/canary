eb2825d14e9fa840c4daf496ac246b856288b9fb
Rasmus Høyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2022-03-14 09:17:23 +0100

    fixed imports for linter

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,12 @@
 import argparse
 from typing import List
 from src.ts.language_library import Language
-from src.ts.syntax import Syntax
 from src.ts.parser import Parser
 from src.ts.tree import Tree
 from src.ts.node import Node
-from utilities.setupCommandLine import setupCommandLine
-from utilities.setupCommandLine import setupIOHandler
+from src.unit_analyser import UnitAnalyser
+from .Utilities import setupCommandLine
+from .Utilities import setupIOHandler
 
 
 def main():
@@ -14,17 +14,13 @@ def main():
     ioHandler = setupIOHandler(commandLineParser)
     parser: Parser = Parser.c()
     language: Language = parser.language
-    syntax: Syntax = language.syntax
     tree: Tree = parser.parse(ioHandler.input_file_text)
-    functions: List[Node] = language.query(
-        syntax.query_function_declaration
-    ).captures(tree.root_node).nodes(syntax.get_function_declaration)
-    structs: List[Node] = language.query(
-        syntax.query_struct_declaration
-    ).captures(tree.root_node).nodes(syntax.get_struct_declaration)
+    unitAnalyser: UnitAnalyser = UnitAnalyser(language, tree.root_node)
+
+    structs = unitAnalyser.get_struct_declarations()
+    functions = unitAnalyser.get_function_declarations()
     languageConstructs: List[Node] = functions.__add__(structs)
     print(languageConstructs)
-
 
 if __name__ == "__main__":
     main()
