f76c04df0dfac0d27a0740e18964746db701a90c
Andreas <akbr18@student.aau.dk>
2022-03-15 10:38:28 +0100

    feature (cutest): Added cutest and an example

---
+++ examples/c_06/src/test.c
@@ -0,0 +1,13 @@
+#include "../../libs/cutest-1.5/CuTest.h"
+
+/* Runs all the CuTest suites */
+void RunAllTests(void);
+
+int main(void) {
+    RunAllTests();
+    return 0;
+}
+
+void RunAllTests(void) {
+
+}
