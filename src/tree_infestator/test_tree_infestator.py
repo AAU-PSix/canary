0e26023980965acab95c36e32973f6510cd8a853
Rasmus Hoeyer Hansen <rhha19@student.aau.dk>
2022-03-21 14:46:48 +0100

    tests (Infestator): added sanity checks to ensure overlap in node syntax is caught

--- src/tree_infestator/test_tree_infestator.py
+++ src/tree_infestator/test_tree_infestator.py
@@ -99,3 +99,40 @@ class TestTreeInfestator(unittest.TestCase):
         self.assertEqual(sorted[0].node.type, "expression_statement")
         self.assertEqual(sorted[1].node.type, "compound_statement")
         self.assertEqual(sorted[2].node.type, "parenthesized_expression")
+
+    def test_else_if_is_alternative_of_if_false(self) -> None:
+        program: str = "if(a) { } else if (b){ }"
+        tree: Tree = self._parser.parse(program)
+        if_else_node: Node = tree.root_node.named_children[0]
+        consequence: Node = if_else_node.child_by_field_name("consequence")
+        
+        expected: bool = False
+        actual = self._infestator.is_alternative_of_if(consequence)
+
+        self.assertEqual(if_else_node.type, "if_statement")
+        self.assertEqual(consequence.type, "compound_statement")
+        self.assertEqual(actual, expected)
+
+
+    def test_else_if_else_is_alternative_of_if_false(self) -> None:
+        program: str = "if(a) { } else if (b){ } else {}"
+        tree: Tree = self._parser.parse(program)
+        else_if_else_node: Node = tree.root_node.named_children[0]
+
+        consequence: Node = else_if_else_node.child_by_field_name("consequence")
+        
+        expected: bool = True
+        actual = self._infestator.is_consequence_of_if(consequence)
+        
+        self.assertEqual(else_if_else_node.type, "if_statement")
+        self.assertEqual(consequence.type, "compound_statement")
+        self.assertEqual(actual, expected)  
+
+    
+
+
+
+
+
+
+
