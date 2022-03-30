905bcfe246b2b3d846bf074abd260f8befc2b271
goldflue <dvalne19@student.aau.dk>
2022-03-30 14:24:34 +0200

    refactor (parser_cutest): refactored parser_cutest, so no static methods. Also added a few tests

--- src/cutest_parser/test_cutest_parser.py
+++ src/cutest_parser/test_cutest_parser.py
@@ -1,5 +1,5 @@
 import unittest
-from src.cutest_parser.parser_cutest import CuTestParser, FailedCuTest, FailedCuTestExpAss, FailedCuTestAss
+from src.cutest_parser.parser_cutest import CuTestParser, FailedCuTest
 
 from . import *
 
@@ -8,21 +8,102 @@ class TestCutestParser(unittest.TestCase):
         return super().setUp()
     
     def test_parse_string_with_colon(self) -> None:
-        # self._parser = CuTestParser()
-        # parsed_line = ["5) Test_CuAssertPtrEquals: /input/tests/AllTests.c:55: expected <Test Hest: Blæst> but was <Pøls: 1 2 3>"]
-        # failed_cutests = self._parser.parse(parsed_line)
+        # Arrange 
+        self._parser = CuTestParser()
+        parsed_line = ["5) Test_CuAssertPtrEquals: /input/tests/AllTests.c:55: expected <Test Hest: Blæst> but was <Pøls: 1 2 3>"]
         
-        # expected_expected = "Test: Hest: Blæst"
+        expected_expected = "Test Hest: Blæst"
+        expected_actual = "Pøls: 1 2 3"
 
-        # expected_actual = "Pøls: 1 2 3"
-
-        # actual_expected = failed_cutests[0].expected
-        # actual_actual = failed_cutests[0].actual
-
-        # self.assertEqual(expected_expected, actual_expected)
-        # self.assertEqual(actual_actual, expected_actual)
-        self.assertTrue(True)
+        # Act 
+        failed_cutests = self._parser.parse(parsed_line)
         
+        actual_expected = failed_cutests[0].expected
+        actual_actual = failed_cutests[0].actual
+
+        # Assert 
+        self.assertEqual(expected_expected, actual_expected)
+        self.assertEqual(actual_actual, expected_actual)
+        
+    def test_parse_int_result(self) -> None:
+        # Arrange 
+        self._parser = CuTestParser()
+        parsed_line = ["5) Test_CuAssertPtrEquals: /input/tests/AllTests.c:55: expected <100> but was <69>"]
+        
+        expected_expected = "100"
+        expected_actual = "69"
+
+        # Act 
+        failed_cutests = self._parser.parse(parsed_line)
+        
+        actual_expected = failed_cutests[0].expected
+        actual_actual = failed_cutests[0].actual
+
+        # Assert 
+        self.assertEqual(expected_expected, actual_expected)
+        self.assertEqual(actual_actual, expected_actual)
+        
+    def test_parse_double_result(self) -> None:
+        # Arrange 
+        self._parser = CuTestParser()
+        parsed_line = ["5) Test_CuAssertPtrEquals: /input/tests/AllTests.c:55: expected <200.00> but was <69.00>"]
+        
+        expected_expected = "200.00"
+        expected_actual = "69.00"
+
+        # Act 
+        failed_cutests = self._parser.parse(parsed_line)
+        
+        actual_expected = failed_cutests[0].expected
+        actual_actual = failed_cutests[0].actual
+
+        # Assert 
+        self.assertEqual(expected_expected, actual_expected)
+        self.assertEqual(actual_actual, expected_actual)
+    
+    def test_parse_pointer_result(self) -> None:
+        # Arrange 
+        self._parser = CuTestParser()
+        parsed_line = ["5) Test_CuAssertPtrEquals: /input/tests/AllTests.c:55: expected pointer <0x0x16c17e0> but was <0x0x16c1800>"]
+        
+        expected_expected = "0x0x16c17e0"
+        expected_actual = "0x0x16c1800"
+
+        # Act 
+        failed_cutests = self._parser.parse(parsed_line)
+        
+        actual_expected = failed_cutests[0].expected
+        actual_actual = failed_cutests[0].actual
+
+        # Assert 
+        self.assertEqual(expected_expected, actual_expected)
+        self.assertEqual(actual_actual, expected_actual)
+    
+    def test_parse_assert_result(self) -> None:
+        # Arrange 
+        self._parser = CuTestParser()
+        parsed_line = ["5) Test_CuAssertGuguGaga: /input/tests/AllTests.c:55: assert failed"]
+        
+        expected_expected = "True"
+        expected_actual = "assert failed"
+
+        # Act 
+        failed_cutests = self._parser.parse(parsed_line)
+        
+        actual_expected = failed_cutests[0].expected
+        actual_actual = failed_cutests[0].actual
+
+        # Assert 
+        self.assertEqual(expected_expected, actual_expected)
+        self.assertEqual(actual_actual, expected_actual)
+
+    # That it does not read wrongful line (no test_name)
+    # That it does not read wrongful line (no test_src)
+    # That it does not read wrongful line (no test_message)
+
+    # Test from based on file
+
+    # Test single_line_parse 
 
 
 
