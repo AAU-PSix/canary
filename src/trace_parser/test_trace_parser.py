fefee74add51b0067537531f88d4e6c5ebd394d6
Tommy <ddieu19@student.aau.dk>
2022-04-06 15:02:22 +0200

    updated test trace parser and added init

--- src/trace_parser/test_trace_parser.py
+++ src/trace_parser/test_trace_parser.py
@@ -10,51 +10,33 @@ class TestTraceParser(unittest.TestCase):
 
         root_node = LocationNode(None, list(), 'AddTest', None, None)
         tree = Tree[LocationNode](root_node)
-
+    
         node1 = LocationNode(None, list(), 'AddTest', 'unit1', 'loc1')
+        node2 = LocationNode(None, list(), 'AddTest', 'unit1', 'loc2')
 
         node1._idStateList.append(('ida','state1'))
-        node1._idStateList.append(('idb','state2'))
-
+        node2._idStateList.append(('idb','state2'))
 
         root_node.children.append(node1)
+        root_node.children.append(node2)
+        root_node.children[-1].children.append(node1)
+        #print(root_node.children[-1].unitName)
+        #print(root_node.children[-1].child_count)
 
-        test_string = ['StartTest=AddTest','EndTest=AddTest']
+        test_string = ['StartTest=AddTest', 'UnitStart=newUnit', 'location=a1', 'location=a2', 'UnitEnd=newUnit', 'EndTest=AddTest']
 
         parser_trace = Parser_trace()
-        #ParseTrace = parser_trace.parse_list(test_string)
+        ParseTrace = parser_trace.parse_list(test_string)
         
         self.assertEqual(tree.root.testName, 'AddTest')
-        #self.assertTrue(len(ParseTrace) != 0)
-        #print(ParseTrace)
+        self.assertTrue(len(ParseTrace) != 0)
+        #print(tree.root.child_count)
+        #print(tree.root.children[0])
+        #print(ParseTrace[0].root.child_count)
+        #print(ParseTrace[0].root.sibling_count)
+        print(ParseTrace[0].root.children[0].sibling_count)
+        print(node1.sibling_count)
 
-
-    # def test_parse_trace_two_units(self) -> None:
-    #     root_node = LocationNode(None, list(), None, None)
-    #     tree = Tree(root_node)
-
-    #     node1 = LocationNode(None, list(), 'test1', 'unit1', 'loc1')
-    #     node2 = LocationNode(None, list(), 'test1', 'unit2', 'loc2')
-    #     node3 = LocationNode(None, list(), 'test1', 'unit1', 'loc3')
-
-    #     node1.idStateList.append(('ida','state1'))
-    #     node1.idStateList.append(('idb','state2'))
-
-    #     node2.idStateList.append(('idc','state3'))
-    #     node2.idStateList.append(('idd','state4'))
-        
-    #     node3.idStateList.append(('ide','state5'))
-    #     node3.idStateList.append(('idf','state6'))
-
-    #     root_node.children.append(node1)
-    #     node1.siblings.append(node2)
-    #     node2.children.append(node3)
-
-    #     test_string = ['StartTest=AddTest', 'UnitStart=unit1', 'location=loc1', 'id=ida', 'state=state1', 'id=idb', 'state=state2', 
-    #     'UnitStart=unit2', 'location=loc2', 'id=idc', 'state=state3', 'id=idd', 'state=state4', 'UnitEnd=unit2', 'EndTest=AddTest']
-
-    #     parser_trace = Parser_trace()
-        
 class LocationNode(Node):
     def __init__(self, parent: TNode, children: List[TNode], testName: str, unitName: str = None, locationName: str = None, ) -> None:
         super().__init__(parent, children)
@@ -85,62 +67,75 @@ class Parser_trace():
     
     def parse_list(self, trace_list: List[str]) -> List[Tree[LocationNode]]:
         
+        #return list
         traceTreeList: List[Tree[LocationNode]] = []
+
+        testTreeStack: List[Tree[LocationNode]] = []
 
         testNameStack: List[str] = []
         unitNameStack: List[str] = []
-        testTreeStack: List[Tree[LocationNode]] = []
+
+        treeRootNodeStack: List[LocationNode] = []
         locationNodeStack: List[LocationNode] = []
-      
+    
         prevTrace : str = ''
         idStateTuple = (None, None)
         
         for trace in trace_list:
             
-            trace.split("=")
+            trace = trace.split("=")
 
             if "StartTest" in trace[0]:
             
                 rootNode = LocationNode(None, list(), trace[1] ,None, None)
-                
-                locationNodeStack.append(rootNode)
-                testNameStack.append([trace[1]])
-         
-                tree = Tree[LocationNode](rootNode)
-                testTreeStack.append(tree)
+
+                testNameStack.append(trace[1])       
+
+                treeRootNodeStack.append(rootNode)
+
+                #testTreeStack.append(tree)
 
             if "EndTest" in trace[0]:
-                # if trace[1] != testTreeStack[-1].root.unitName:
-                #     raise Exception('Test has not been ended correctly')
-                traceTreeList.append(testTreeStack[-1])
+                if trace[1] != testNameStack[-1]:
+                    raise Exception('Test has not been ended correctly')
+
+                tree = Tree[LocationNode](treeRootNodeStack[-1])
+                tree.root.children.append(locationNodeStack[-1])
+
                 testNameStack.pop()
+                #traceTreeList[-1].root.children.append()
+                traceTreeList.append(tree)
                 #locationStack = []
 
-            # if "UnitStart" in trace[0] and trace[1] not in unitNameStack[-1]:
-            #     unitNameStack.append(trace[1])
+            if "location" in trace[0]:
                 
-            # if "UnitEnd" in trace[0]:
-            #     if trace[1] == unitNameStack[-1]:
-            #         unitNameStack.pop()
-            #     else:
-            #         raise Exception('locationStack top element does not match with UnitEnd')                
+                if testNameStack[-1] is None:
+                    raise Exception('top element in testNameStack is None')
+
+                if unitNameStack[-1] is None:
+                    raise Exception('top element in unitNameStack is None')
+
+                if trace[1] is None:
+                    raise Exception('value from trace is None')  
+
+                newlocationNode = LocationNode(None, list(), testNameStack[-1], unitNameStack[-1], trace[1])
+                
+                locationNodeStack.append(newlocationNode)
+
+                if newlocationNode.unitName != unitNameStack[-1]:
+                    locationNodeStack[-1].children.append(newlocationNode)
+                else:
+                    locationNodeStack[-1].siblings.append(newlocationNode)
+
+            if "UnitStart" in trace[0]:
+                unitNameStack.append(trace[1])
+                
+            if "UnitEnd" in trace[0]:
+                if trace[1] == unitNameStack[-1]:
+                    unitNameStack.pop()
+                else:
+                    raise Exception('locationStack top element does not match with UnitEnd')                
             
-            # if "location" in trace[0]:
-                
-            #     if locationNodeStack[-1] is None:
-            #         raise Exception('Stack containing location nodes is empty')
-
-            #     if trace[1] == locationNodeStack[-1].locationName:
-            #         raise Exception('sequantial location are idential')
-
-            #     locationNode = LocationNode(testNameStack[-1], unitNameStack[-1], trace[1])
-            #     locationNodeStack.append(locationNode)
-
-            #     if locationNode.unitName == unitNameStack:
-            #         locationNodeStack[-1].siblings.append(locationNode)
-            #     else:
-            #         locationNodeStack[-1].children.append(locationNode)
-
             # if "id" in trace[0]:
             #     if not idStateTuple:
             #         raise Exception('Id_state tuple is not empty!')
